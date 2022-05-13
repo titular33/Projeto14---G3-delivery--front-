0bb9e7111cda41bb8ad4b9073a518ac6ad781d76
amorim.pedro@engenharia.ufjf.br <amorim.pedro@engenharia.ufjf.br>
2022-05-12 23:59:07 -0300

    Add drinks router, and back cart

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ import chalk from "chalk";
 import cors from "cors";
 
 import authRouter from "./routes/authRouter";
+import drinksRouter from "./routes/drinksRouter";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(json());
-app.use(authRouter);
+app.use("/auth", authRouter);
+app.use("/drinks", drinksRouter);
+
 
 app.listen(process.env.PORT, () => {
     console.log(chalk.bold.green(`Server is running at ${process.env.PORT}`))
