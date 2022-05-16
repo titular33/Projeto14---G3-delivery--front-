de179879b62271adfe8272b4c9638c778ec646e7
matheusfacciolla <matheustassifacciolla@gmail.com>
2022-05-16 05:31:35 -0300

    feat: delete cart endpoint

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,18 +1,13 @@
 import db from "./../db.js";
+import { ObjectId } from "mongodb";
 
 export async function addProduct(req, res) {
   const { user } = res.locals;
   const { name, idProduct, brand, image, price } = req.body;
 
-  console.log("USER", user)
-  console.log("INFOSREQ.BODY", req.body)
-  console.log("INFOSDRINK", name, idProduct, brand, image, price)
-
   try {
-    await db.collection("carts").insertOne({...req.body, idUser: user._id});
-
-    console.log("USERID", user._id)
-    res.send({idUser: user._id}).status(200);
+    await db.collection("carts").insertOne({ ...req.body, idUser: user._id });
+    res.send({ idUser: user._id }).status(200);
 
   } catch (error) {
     res.status(500).send("Falha no addProduct " + error);
@@ -22,8 +17,6 @@ export async function addProduct(req, res) {
 export async function productsCart(req, res) {
   try {
     const userCart = await db.collection("carts").find({}).toArray();
-
-    console.log("USERCART", userCart)
     res.send(userCart).status(200);
 
   } catch (error) {
@@ -32,18 +25,28 @@ export async function productsCart(req, res) {
 }
 
 export async function finishOrder(req, res) {
-  const { user } = res.locals;
   const { cartProducts } = req.body;
 
-  console.log("USER", user)
-  console.log("INFOSREQ.BODY", req.body)
-  console.log("cartProducts", cartProducts)
-
   try {
-    await db.collection("sold").insertMany({cartProducts});
+    await db.collection("solds").insertMany({ cartProducts });
     res.sendStatus(200);
 
   } catch (error) {
     res.status(500).send("Falha no finishOrder " + error);
+  }
+}
+
+export async function deleteProducts(req, res) {
+  const { idProduct } = req.body;
+
+  const product = await db.collection("drinks").findOne({ _id: idProduct })
+
+  try {
+    await db.collection("drinks").deleteOne({ product });
+    res.sendStatus(200);
+
+  } catch (error) {
+    res.status(500).send(error);
+    console.log(error);
   }
 }
